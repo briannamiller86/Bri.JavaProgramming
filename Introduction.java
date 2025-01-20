a4de333ccb211399d93ced4cbe1ce4873dd26eaa
Brianna Miller <handykidsmiller@hotmail.com>
2025-01-19 23:53:33 -0500

    Initial commit

---
+++ Introduction.java
@@ -0,0 +1,16 @@
+/*
+Brianna Miller
+COP-2800-98672 Java Programming
+1/19/2025
+The code below will execute simple print statements responding to "D1 - First Java program for this course"
+No AI was used to complete this assignment.
+ */
+public class Introduction {
+    public static void main(String[] args){
+       System.out.println("Hello, my name is Brianna Miller (Bri for short).");
+       System.out.println("I am majoring in Computer Programming and Analysis.");   
+       System.out.println("This is my third year dual enrolled at HCC.");
+       System.out.println("Oddly enough, my ultimate career goal is to become an ECU tuner for cars.");     
+       System.out.println("Fun fact: I am currently building my 2009 BMW e92 M3.");
+    }
+ }
